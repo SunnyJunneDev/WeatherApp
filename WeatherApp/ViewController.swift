29f2c88e42b69336c7c69a1d3ef3e2c67a81514f
Svetlana Shardakova <svetlana.mikhaljova@gmail.com>
2020-06-08 14:09:33 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Светлана Шардакова on 08.06.2020.
+//  Copyright © 2020 Светлана Шардакова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
