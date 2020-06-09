0c4874d849c9bb35d448893240c65bf7b5b2ef55
Svetlana Shardakova <svetlana.mikhaljova@gmail.com>
2020-06-09 11:04:48 +0300

    main folder has been added

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Светлана Шардакова on 08.06.2020.
+//  Copyright © 2020 Светлана Шардакова. All rights reserved.
+//
+
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let icon: UIImage
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+   
+    @IBAction func refreshButtonPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let icon = WeatherIconManager.Rain.image
+        let currentWeather = CurrentWeather(temperature: 10.0, appearentTemperature: 5.0, humidity: 30, pressure: 250, icon: icon)
+        updateUIWith(currentWeather: currentWeather)
     }
-
-
+    
+    func updateUIWith(currentWeather: CurrentWeather) {
+        self.imageView.image = currentWeather.icon
+        self.pressureLabel.text = "\(currentWeather.pressure)"
+        self.temperatureLabel.text = "\(currentWeather.temperature)"
+        self.appearentTemperatureLabel.text = "\(currentWeather.appearentTemperature)"
+        self.humidityLabel.text = "\(currentWeather.humidity)"
+        
+        
+        
+        
+    }
 }
 
